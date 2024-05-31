9e43022c52e97c328856f44099f256801f4675a6
Kai <142235179+xendsa@users.noreply.github.com>
2024-05-31 19:12:17 +0700

    Initial Commit

---
+++ navbar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="navbarstyle.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Taman Wanasutan Asri</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="dashboard.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" rel="stylesheet" href="pages/arsip.php">Arsip</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" rel="stylesheet" href="pages/floraFauna.php">Flora Fauna</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" rel="stylesheet" href="pages/dataBuku.php">Poca Buku</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</body>
+</html>
--- pages/floraFauna.php
+++ pages/floraFauna.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flora Fauna Page</title>
-    <link href="cssflorafauna.css" rel="stylesheet">
+    <link rel="stylesheet" href="cssflorafauna.css">
 </head>
 <body>
     <div class="top-action-bar">
         <div class="logo">
             <a href="#">Logo</a>
         </div>
-        <div class="Judul"></div>
+        <div class="Judul">
+            <li></li>
+        </div>
     </div>
 </body>
 </html>
