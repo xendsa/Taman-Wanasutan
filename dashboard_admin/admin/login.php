8df6404eb0f529e01dbdb5327613a0b7a1960ce4
yall <senseialegaasptr@gmail.com>
2024-06-01 23:06:26 +0700

    rizal kocak

--- dashboard_admin/admin/login.php
+++ dashboard_admin/admin/login.php
@@ -2,31 +2,32 @@
 	session_start();
 	include 'includes/conn.php';
 
-	if(isset($_POST['login'])){
+	if (isset($_POST['login'])) {
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 
-		$sql = "SELECT * FROM admin WHERE username = '$username'";
-		$query = $conn->query($sql);
+		// Use prepared statements to prevent SQL injection
+		$sql = "SELECT * FROM admin WHERE username = ? AND password = ?";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param('ss', $username, $password);
+		$stmt->execute();
+		$query = $stmt->get_result();
 
-		if($query->num_rows < 1){
+		if ($query->num_rows < 1) {
 			$_SESSION['error'] = 'Cannot find account with the username';
-		}
-		else{
+		} else {
 			$row = $query->fetch_assoc();
-			if($username ==  $row['username']){
+			if ($username == $row['username']) {
 				$_SESSION['admin'] = $row['id'];
-			}
-			else{
+			} else {
 				$_SESSION['error'] = 'Incorrect password';
 			}
 		}
 		
-	}
-	else{
+		$stmt->close();
+	} else {
 		$_SESSION['error'] = 'Input admin credentials first';
 	}
 
 	header('location: index.php');
-
 ?>
