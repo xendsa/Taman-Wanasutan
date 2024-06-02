3f12330bd3c431cfbae3a0d5d0b3ea28ec0c9bff
Fadel Fitrah <fansurifadelfitra@gmail.com>
2024-06-02 22:35:56 +0700

    first commit

--- pages/arsip/arsip.php
+++ pages/arsip/arsip.php
@@ -13,7 +13,7 @@
     rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  <script cript src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
 
@@ -109,8 +109,14 @@
 
   </div>
 
-  <div class="all-arsip">
+  <div class="all-arsips">
+    <div class="arsip-kegiatan">
 
+    </div>
+
+    <div class="arsip-kompos">
+
+    </div>
   </div>
 
   <script src="js/script.js"></script>
--- pages/arsip/js/script.js
+++ pages/arsip/js/script.js
@@ -1,12 +1,15 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarCollapse = document.querySelector(".navbar-collapse");
 
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
-
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1])
-})
+  navbarToggler.addEventListener("click", function () {
+    if (!navbarCollapse.classList.contains("show")) {
+      setTimeout(() => {
+        window.scrollTo({
+          top: navbarCollapse.offsetTop,
+          behavior: "smooth",
+        });
+      }, 300);
+    }
+  });
+});
