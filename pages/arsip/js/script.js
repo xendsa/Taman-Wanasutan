8d37bfbbcc5a37068a7325057bc083e81a36bd86
Kai <142235179+xendsa@users.noreply.github.com>
2024-06-02 22:33:13 +0700

    test

--- pages/arsip/js/script.js
+++ pages/arsip/js/script.js
@@ -1,12 +1,30 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+// Image Slider Functionality
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
 
 next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[0]);
+});
 
 prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1])
-})
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').prepend(items[items.length - 1]);
+});
+
+// Navbar Collapsing Functionality
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navbarToggler.addEventListener('click', function() {
+        if (!navbarCollapse.classList.contains('show')) {
+            setTimeout(() => {
+                window.scrollTo({
+                    top: navbarCollapse.offsetTop,
+                    behavior: 'smooth'
+                });
+            }, 300); // Sesuaikan dengan waktu transisi
+        }
+    });
+});
