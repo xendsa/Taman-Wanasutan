6d99b94b1a492e6246696077f7fd1b022286b7a0
Kai <142235179+xendsa@users.noreply.github.com>
2024-06-23 23:45:46 +0700

    popup item

--- pages/flora-fauna/js/script.js
+++ pages/flora-fauna/js/script.js
@@ -1,9 +1,12 @@
-document.querySelectorAll('.flora img').forEach(image => {
+document.querySelectorAll('.flora img, .fauna img').forEach(image => {
+  console.log(image)
   image.onclick = () => {
+    document.querySelector('.item-preview').style.display = 'block';
     document.querySelector('.item-preview .preview').style.display = 'block';
     document.querySelector('.item-preview .preview img').src = image.getAttribute('src');
   }
 });
-document.querySelector('.preview span').onclick = () => {
-  document.querySelector('.preview').style.display = 'none';
+document.querySelector('.item-preview .preview span').onclick = () => {
+  document.querySelector('.item-preview').style.display = 'none';
+  document.querySelector('.item-preview .preview').style.display = 'none';
 }
