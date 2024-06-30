99ac271f04d46e86b8d99e94ec4c69faccfc52f4
Kai <142235179+xendsa@users.noreply.github.com>
2024-06-30 20:20:03 +0700

    apply flora/fauna db

---
+++ pages/flora-fauna/db.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "taman-asri";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM fauna";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo '<div class="grid-item">';
+        echo '<img src="'.$row['gambar'].'" alt="'.$row['nama'].'">';
+        echo '<div class="description">';
+        echo '<h3>'.$row['nama'].'</h3>';
+        echo '<p>'.$row['deskripsi'].'</p>';
+        echo '</div>';
+        echo '</div>';
+    }
+} else {
+    echo "No images found.";
+}
+
+$conn->close();
+
+?>
