f89f4cf760e1c5bfa3e380fa3c72188d9865f6eb
Fadel Fitrah <fansurifadelfitra@gmail.com>
2024-10-28 14:06:03 +0700

    hamdalah

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -17,12 +17,8 @@ export default function AppRoutes() {
         <Route path="flora-fauna" element={<FloraFaunaPages />} />
         <Route path="arsip" element={<ArsipPages />} />
         <Route path="flora-fauna/detail/:slug" element={<DetailFloraPages />} />
-<<<<<<< HEAD
         <Route path="arsip/detail/:id" element={<DetailArsipPages />} />
-=======
-        <Route path="arsip/detail/:slug" element={<DetailArsipPages />} />\
-        <Route path="poca" element={<PocaPage />} />\
->>>>>>> 70005c5dfa274951d7641e29c3dea5c5916a2f32
+        <Route path="poca" element={<PocaPage />} />
       </Route>
     </Routes>
   );
